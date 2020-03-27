559af8f1a0a7676b7825fb4dcd3fceb0147f250d
3110445 <43026116+SaitoYoshiko@users.noreply.github.com>
2020-03-27 21:29:24 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -19,9 +19,9 @@ assessmentButton.onclick = () => {
         return;
     }
     
-    removeAllChildren(resultDicided);
+    removeAllChildren(resultDivided);
     const header = document.createElement('h3');
-    headr.innerText ='診断結果'
+    header.innerText ='診断結果'
     resultDivided.appendChild(header);
     
     const paragraph = document.createElement('p');
@@ -47,21 +47,21 @@ assessmentButton.onclick = () => {
 };
 
 cosnt answers = [
-    '{userName}のいいところは声です。{userName}の特徴的な声は皆を引きつけ、心に残ります。'
-    '{userName}のいいところは眼差しです。{userName}に見つめられた人は気になって仕方がないでしょう。'
-    '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。'
-    '{userName}のいいところは厳しさです。{userName}の厳しさがもの事をいつも成功に導きます。'
-    '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。'
-    '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。'
-    '{userName}のいいところは用心深さです。{userName}の洞察に多くの人が助けられます。'
-    '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。'
-    '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。'
-    '{userName}のいいところは思いやりです。{userName}に気にかけてもらった多くの人が感謝しています。'
-    '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。'
-    '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。'
-    '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。'
-    '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。'
-    '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。'
+    '{userName}のいいところは声です。{userName}の特徴的な声は皆を引きつけ、心に残ります。',
+    '{userName}のいいところは眼差しです。{userName}に見つめられた人は気になって仕方がないでしょう。',
+    '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
+    '{userName}のいいところは厳しさです。{userName}の厳しさがもの事をいつも成功に導きます。',
+    '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
+    '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
+    '{userName}のいいところは用心深さです。{userName}の洞察に多くの人が助けられます。',
+    '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
+    '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
+    '{userName}のいいところは思いやりです。{userName}に気にかけてもらった多くの人が感謝しています。',
+    '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
+    '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
+    '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
+    '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
+    '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
     '{userName}のいいところは自制心です。やばいと思った時にしっかり衝動を抑えられる{userName}が皆に評価されています。'
     ];
     
@@ -80,7 +80,7 @@ function assessment(userName) {
     
     //名前の合計数を回答の数で割って添字を求める
     const index = sumOfCharCode % answers.length;
-    let result = answers[inde];
+    let result = answers[index];
     
     result = result.replace(/{userName}/g, userName);
     return result;
